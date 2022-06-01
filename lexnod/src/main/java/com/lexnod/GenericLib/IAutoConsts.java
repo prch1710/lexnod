447b9ef6aa3747a56680978e07b6a25b37f3e238
PRIYANKA <PRIYANKA@DESKTOP-OTSD4BU>
2022-06-01 16:43:51 +0530

    path commit

--- lexnod/src/main/java/com/lexnod/GenericLib/IAutoConsts.java
+++ lexnod/src/main/java/com/lexnod/GenericLib/IAutoConsts.java
@@ -3,12 +3,12 @@ package com.lexnod.GenericLib;
 public interface IAutoConsts {
 
 	String CHROME_KEY="webdriver.chrome.driver";
-	String CHROME_VALUE="C:\\Users\\PRIYANKA\\eclipse-workspace\\lexnod\\src\\main\\resources\\driver\\chromedriver.exe";
+	String CHROME_VALUE="C:\\Users\\PRIYANKA\\git\\repository\\lexnod\\src\\main\\resources\\driver\\chromedriver.exe";
 	
 	String GECKO_KEY="webdriver.gecko.driver";
-	String GECKO_VALUE="C:\\Users\\PRIYANKA\\eclipse-workspace\\lexnod\\src\\main\\resources\\driver\\geckodriver.exe";
+	String GECKO_VALUE="C:\\Users\\PRIYANKA\\git\\repository\\lexnod\\src\\main\\resources\\driver\\geckodriver.exe";
 	
-	String PROP_PATH="./src/test/resources/dataRepo/config.properties";
-	String EXCEL_PATH="./src/test/resources/dataRepo/inputData.xlsx";
+	String PROP_PATH="C:\\Users\\PRIYANKA\\git\\repository\\lexnod\\src\\test\\resources\\dataRepo\\config.properties";
+	String EXCEL_PATH="C:\\Users\\PRIYANKA\\git\\repository\\lexnod\\src\\test\\resources\\dataRepo\\inputData.xlsx";
 	
 }
